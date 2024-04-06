e3fbd1cb1895ad7575735ce4d1cbdba025b2215d
drimag <rafaelvedimagiba@gmail.com>
2024-04-07 04:08:59 +0800

    initial setup

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const dbService = require('./dbService');
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended : false }));
+
+
+// create
+
+// read
+
+// update
+
+// delete
+
+app.listen(process.env.PORT, () => console.log('app is running'));
